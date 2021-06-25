7a855c6f8f0350628693e07d04902c5e7319a2cb
Chase Wohrle <67084572+ChaseWohrle@users.noreply.github.com>
2021-06-25 13:27:16 -0700

    Update main.tf

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "" # Enter the Terraform Cloud Organization here
+    organization = "Caliber-Global-Solutions" # Enter the Terraform Cloud Organization here
 
     workspaces {
       name = "ecr-aws-circleci"
